806ebb4521a2afededef867c22e833ff061360b4
Abhinav <79625167+Techguy47@users.noreply.github.com>
2024-03-12 21:30:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Select the element with the ID "message"
+var messageElement = document.getElementById("message");
+
+// Change the content of the element when the page loads
+messageElement.textContent = "This text is added by JavaScript!";
+
+// Function to change the background color on button click
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = "lightblue";
+}
